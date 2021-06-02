e97547b4c5f6501c5da3e530c18107456ce85f28
abhishekdharmani <84760205+abhishekdharmani@users.noreply.github.com>
2021-06-02 10:28:27 +0530

    Add files via upload

---
+++ Day4/Dynamic side 1/SocialMedia.php
@@ -0,0 +1,4 @@
+<ul class="social-icons">
+                <li><a href="https://www.facebook.com/abhishek.m.dharmani/"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/abhi_dharmani/?utm_medium=copy_link"><i class="fa fa-instagram"></i></a></li>
+              </ul>
