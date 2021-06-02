beb64a193e2eabe72dc946ebe72c2f412f3a1bbd
abhishekdharmani <84760205+abhishekdharmani@users.noreply.github.com>
2021-06-02 10:17:18 +0530

    Add files via upload

---
+++ Day4/Dynamic side 1/footer.php
@@ -0,0 +1,13 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+                <p>Copyright &copy; 2021 Abhis Cloths</p>
+            
+            - Design: <a rel="nofollow noopener" href="http://AbhiVlog.tk/" target="_blank">Abhishek Vlog</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ Day4/Themepart/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+	<p>Copy Rigths 2021. Website Manage BY ABHISHEK DHARMANI</p>
+</div>
