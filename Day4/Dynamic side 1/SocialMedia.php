0038f25cb83160b3c4fc55b624165ab0f1660ed4
abhishekdharmani <84760205+abhishekdharmani@users.noreply.github.com>
2021-06-02 10:27:45 +0530

    Add files via upload

---
+++ Day4/Dynamic side 1/SocialMedia.php
@@ -0,0 +1,4 @@
+<ul class="social-icons">
+                <li><a href="https://www.facebook.com/abhishek.m.dharmani/"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://twitter.com/priyaldalal786"><i class="fa fa-behance"></i></a></li>
+              </ul>
