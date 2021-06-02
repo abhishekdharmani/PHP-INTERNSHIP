135e820f44f0b8be749ea865863f5a79bc2279ee
abhishekdharmani <84760205+abhishekdharmani@users.noreply.github.com>
2021-06-02 10:27:28 +0530

    Delete SocialMedia.php

--- Day4/Dynamic side 1/SocialMedia.php
+++
@@ -1,4 +0,0 @@
-<ul class="social-icons">
-                <li><a href="https://www.facebook.com/abhishek.m.dharmani/"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="https://twitter.com/priyaldalal786"><i class="fa fa-behance"></i></a></li>
-              </ul>
